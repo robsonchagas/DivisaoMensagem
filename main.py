413f99b4c04bb7b744a50919c1cf9604b35fe8b2
Robson Chagas <112735714+robsonchagas@users.noreply.github.com>
2024-03-19 22:07:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+valor1 = int(input("Insira um valor:"))
+valor2 = int(input("Insira outro valor:"))
+if valor2 == 0:
+  print("ERRO! Não é possível fazer a divisão")
+else:
+  divisao = valor1 / valor2
+  print("O resultado da divisão é:", divisao)
